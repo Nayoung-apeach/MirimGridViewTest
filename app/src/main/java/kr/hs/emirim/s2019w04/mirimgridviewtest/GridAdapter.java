89f4f0d43469bf95cb40160c68ddd5150d177162
Nayoung-apeach <s2019w04@e-mirim.hs.kr>
2020-12-01 20:44:20 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/s2019w04/mirimgridviewtest/GridAdapter.java
@@ -0,0 +1,46 @@
+package kr.hs.emirim.s2019w04.mirimgridviewtest;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class GridAdapter extends BaseAdapter {
+    Context context;
+    int[] imgRes = {R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e,
+            R.drawable.f,R.drawable.g,R.drawable.h,R.drawable.i,R.drawable.j,
+            R.drawable.k,R.drawable.l,R.drawable.m,R.drawable.n,R.drawable.o,R.drawable.p
+    };
+
+    public GridAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imgv = new ImageView(context);
+        imgv.setLayoutParams(new GridView.LayoutParams(300,400));
+        imgv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        imgv.setPadding(5,5,5,5);
+        imgv.setImageResource(imgRes[position]);
+        return imgv;
+    }
+}
